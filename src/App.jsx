00325d800722a1ec3575839ae133dea2aa681832
Satyam Singh <satyamrajput7239@gmail.com>
2025-07-26 14:24:19 +0530

    feat : add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import NavBar from "./components/NavBar";
+
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div style={{ backgroundColor: "#f2fafe" }}>
+      <NavBar />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "../styles/navBar.css";
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">gradnext</div>
+
+      <ul className="nav-links">
+        <li>
+          <a href="#">
+            Home
+            <span className="underline"></span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            1:1 Coaching Program
+            <span className="underline"></span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Consulting Cohort 101
+            <span className="underline"></span>
+          </a>
+        </li>
+        <li className="dropdown">
+          Book a 45-min Session
+          <span className="arrow">&#9662;</span>
+          <ul className="dropdown-menu">
+            <li>Mock Interview</li>
+            <li>Resume Review</li>
+            <li>Career Counseling</li>
+          </ul>
+        </li>
+      </ul>
+
+      <div className="nav-right">
+        <button className="casebuddy-btn">CaseBuddy</button>
+
+        <div className="login">
+          <button className="login-btn">Log In</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
