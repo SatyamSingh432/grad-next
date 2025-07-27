1513796fd5e607813e456ab36a9bcb02e1d98d71
Satyam Singh <satyamrajput7239@gmail.com>
2025-07-27 09:28:30 +0530

    feat : add icons in hero section

--- src/components/HeroSec.jsx
+++ src/components/HeroSec.jsx
@@ -3,6 +3,10 @@ import React from "react";
 import mckinseyLogo from "../assets/mckinsey.avif";
 import bcgLogo from "../assets/bcg.avif";
 import bainLogo from "../assets/bain.avif";
+import meeting from "../assets/meeting.png";
+import peers from "../assets/peers.png";
+import self from "../assets/self.png";
+
 import "../styles/HeroSec.css";
 export default function HeroSection() {
   return (
@@ -26,14 +30,19 @@ export default function HeroSection() {
 
       <div className="hero-cta">
         <div className="cta-card">
+          <img src={meeting} alt="" />
           <h3>1:1 Coaching Program</h3>
           <p>Tailored 1:1 program for personalized preparation</p>
         </div>
         <div className="cta-card">
+          <img src={peers} alt="" />
+
           <h3>Consulting Cohort 101</h3>
           <p>Consulting foundations with global like-minded peers</p>
         </div>
         <div className="cta-card">
+          <img src={self} alt="" />
+
           <h3>CaseBuddy</h3>
           <p>Self-practice cases at your own pace</p>
         </div>
